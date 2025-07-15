d61c008e7c460c0d0d7716d6e348a63a78d1632e
tomaszmaslon <tomasz.maslon@gmail.com>
2025-07-15 21:02:57 +0200

    Working referied type for lazy initialization

--- src/main/kotlin/dsl/DslLazyExample.kt
+++ src/main/kotlin/dsl/DslLazyExample.kt
@@ -1,51 +1,69 @@
 package org.example.dsl
 
+import org.example.dsl.LazyConfiguration.ConfigurationsMapBuilder
+import kotlin.reflect.KProperty
+
 data class SomeConfiguration(val name: String)
 data class SomeOtherConfiguration(val age: Int)
 
-data class LazyConfigurationMap(val underLaying: MutableMap<Class<out Any>, Lazy<Any>>)
 
-inline fun <reified Configuration : Any> LazyConfigurationMap.get(): Lazy<Configuration> =
-    this.underLaying[Configuration::class.java] as? Lazy<Configuration>
-        ?: throw IllegalArgumentException("No configuration available for key")
+data class LazyConfiguration private constructor(private val underLaying: MutableMap<Class<out Any>, Lazy<Any>>) :
+    MutableMap<Class<out Any>, Lazy<Any>> by underLaying {
 
-inline fun <reified Configuration : Any> ConfigurationsMapBuilder.addLazy(addLazyMethod: ConfigurationsMapBuilder.() -> Lazy<Configuration>) {
-    this.configurations.underLaying[Configuration::class.java] = addLazyMethod()
-}
+    @DslMarker
+    annotation class LazyConfiguration
 
-class ConfigurationsMapBuilder {
-    val configurations = LazyConfigurationMap(mutableMapOf())
+    @LazyConfiguration
+    class ConfigurationsMapBuilder {
+        val configurations = LazyConfiguration(mutableMapOf())
 
-    fun build(): LazyConfigurationMap {
-        return configurations
+        fun build(): org.example.dsl.LazyConfiguration {
+            return configurations
+        }
     }
 }
 
+inline operator fun <reified Configuration : Any> LazyConfiguration.getValue(
+    thisRef: Any?,
+    property: KProperty<*>
+): Configuration = (this[Configuration::class.java] as? Lazy<Configuration>)?.value
+    ?: throw IllegalArgumentException("No configuration available for key")
 
-fun buildConfigurationsMap(init: ConfigurationsMapBuilder.() -> Unit): LazyConfigurationMap {
+inline fun <reified Configuration : Any> ConfigurationsMapBuilder.addLazy(noinline initializer: () -> Configuration) {
+    this.configurations[Configuration::class.java] = lazy(initializer)
+}
+
+fun buildConfigurationsMap(init: ConfigurationsMapBuilder.() -> Unit): org.example.dsl.LazyConfiguration {
     val builder = ConfigurationsMapBuilder()
     builder.init()
     return builder.build()
 }
 
-
-class ComponentBuilder(private val lazyConfigurationMap: LazyConfigurationMap) {
-    val someConfiguration: SomeConfiguration by lazyConfigurationMap.get()
-    val someOtherConfiguration: SomeOtherConfiguration by lazyConfigurationMap.get()
+class ComponentBuilder(private val lazyConfigurationMap: LazyConfiguration) {
+    val someConfiguration: SomeConfiguration by lazyConfigurationMap
+    val someOtherConfiguration: SomeOtherConfiguration by lazyConfigurationMap
 
     fun build() {
+        println("Uruchomienie programu")
         println(someConfiguration.name)
         println(someOtherConfiguration.age)
+        println(someConfiguration.name)
     }
 }
+
 
 object MainDslLazy {
 
     fun main() {
         val configurationsMap = buildConfigurationsMap {
-            addLazy { lazy { SomeConfiguration("Tomek") } }
-            addLazy { lazy { SomeOtherConfiguration(40) } }
+            addLazy {
+                println("Akcja wykonana raz")
+                SomeConfiguration("Tomek")
+            }
+            addLazy { SomeOtherConfiguration(40) }
         }
+
+//        LazyConfiguration(mapOf())
 
 //        val configurationsMap = LazyConfigurationMap(mutableMapOf())
 //        configurationsMap.put(lazy { SomeConfiguration("Tomek") })
